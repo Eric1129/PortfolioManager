18c4d0326b9a36df7c855c4193c25c67d397da0a
Vidushi <vidzimohan@yahoo.co.in>
2021-08-25 15:53:04 +0000

    commit create account component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateaccountComponent } from './CreateAccount/createaccount.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'create-account', component:CreateaccountComponent},
+  {path:'createaccount',component:CreateaccountComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
