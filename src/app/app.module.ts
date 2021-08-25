1ed6eae39e39c6716fd8390a4e7b064238a6f72a
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-25 15:49:42 -0400

    net worth works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ManageaccountsComponent } from './ManageAccounts/manageaccounts.component';
-import { HttpClientModule } from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
