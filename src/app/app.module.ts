10bb5380637cdebb86fc948e724b3162f9533b33
Abdul <arules14@gmail.com>
2021-08-25 18:26:01 +0000

    updates to app component and api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateaccountComponent } from './CreateAccount/createaccount.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CreateaccountComponent } from './CreateAccount/createaccount.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+
+  getAllInvestmentAccounts(){
+    return this.http.get("http://localhost:8000/investment/all");
+  }
+
+  getAllCashAccounts(){
+    return this.http.get("http://localhost:8000/investment/all");
+  }
+}
