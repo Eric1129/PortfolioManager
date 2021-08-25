3fcf0d13080971e6b01c0f30430845c76e9add91
Abdul <arules14@gmail.com>
2021-08-25 18:00:38 +0000

    changes to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PortfolioManager';
+  cashAccounts = [
+    {accountNumber: 45234242},
+    {accountNumber: 44444563},
+    {accountNumber: 53893589},
+    {accountNumber: 54743353}
+
+  ];
+  investmentAccounts = [
+    {accountNumber: 53235255},
+    {accountNumber: 45246654},
+    {accountNumber: 43262362},
+    {accountNumber: 14365425}
+  ];
+
+  @Input() account_number:number = 0;
+  @Input() value:number = 0;
+  @Input() account_type:string = "";
+
+  onChange($event:any){
+    this.account_number = $event.target.options[$event.target.options.selectedIndex].text;
+  }
 }
