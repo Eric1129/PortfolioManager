166f207a8d9cc6a87a167bef2c262c353caf31ab
Tingyu Shen <tingyu.shen@mail.mcgill.ca>
2021-08-26 09:49:30 -0400

    update cash flow

---
+++ src/app/cash-flow/cash-flow.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cash-flow',
+  templateUrl: './cash-flow.component.html',
+  styleUrls: ['./cash-flow.component.css']
+})
+export class CashFlowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('here I am!')
+  }
+
+}
