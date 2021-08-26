b3334ff6dc5ceed8bcb5b933edb4f98763bef931
Abdul <arules14@gmail.com>
2021-08-26 14:41:47 +0000

    staged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,6 @@ import { ApiService } from './services/api.service';
 })
 export class AppComponent {
   constructor(private apiService: ApiService) { 
-
-
-      this.apiService.getAllCashAccounts()
-      .subscribe( (data: any) => {
-        this.cashAccounts = data
-      } )
-  
-      this.apiService.getAllInvestmentAccounts()
-      .subscribe( (data: any) => {
-        this.investmentAccounts = data
-      } )
       this.retriveNetWorth()
       this.retriveTotalInvestmentValue()
       this.retriveTotalCashValue()
@@ -26,31 +15,10 @@ export class AppComponent {
   title = 'PortfolioManager';
   today = new Date();
   
-  cashAccounts = [
-    {accountNumber: 45234242},
-    {accountNumber: 44444563},
-    {accountNumber: 53893589},
-    {accountNumber: 54743353}
-
-   ];
-   
-  investmentAccounts = [
-    {accountNumber: 53235255},
-    {accountNumber: 45246654},
-    {accountNumber: 43262362},
-    {accountNumber: 14365425}
-  ];
 
   @Input() netvalue = 0;
   @Input() totalcashvalue = 0;
   @Input() totalinvestmentvalue = 0;
-  @Input() account_number:number = 0;
-  @Input() account_value:number = 0;
-  @Input() account_type:string = "";
-
-  onChange($event:any){
-    this.account_number = $event.target.options[$event.target.options.selectedIndex].text;
-  }
 
   retriveNetWorth(){
     // we call the service method by subscribing to it
@@ -82,20 +50,4 @@ export class AppComponent {
       } )
   }
 
-  
-  retrieveBasicInvestmentData(){
-    this.account_type = "investment"
-    this.apiService.getInvestmentAccountValue({account_number: this.account_number})
-    .subscribe( (data:any) => {
-      this.account_value = data
-    })
-  }
-
-  retrieveBasicCashData(){
-    this.account_type = "cash"
-    this.apiService.getCashAccountValue({account_number: this.account_number})
-    .subscribe( (data:any) => {
-      this.account_value = data
-    })
-  }
 }
